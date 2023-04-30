38a3e141b2655bbd64dd7ab844408c8d18956652
Dirk Gottschalk <dirk.gottschalk1980@googlemail.com>
2023-04-30 12:11:20 +0200

    Change behaviour  of -n adn set count of loops instead of bottles.

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@
 void help(void)
 {
 	printf("Available Options:\n\n");
-	printf("	-n <number>	Number of Bottles on the wall.\n");
+	printf("	-n <number>	Number of loops.\n");
 	printf("	-d <delay>	Delay betwen the verses in seconds.\n\n");
 	printf("	-h		Show this help.\n\n");
 }
@@ -26,15 +26,17 @@ void help(void)
 int main(int argc, char **argv)
 {
 	int b;
-	int num = 99;
+	int loops;
 	int sleeptime;
-
 	int opt;
+
+	loops = 1;
+	sleeptime = 0;
 
 	while ((opt = getopt(argc, argv, "n:d:hv")) != -1) {
 		switch (opt) {
 		case 'n':
-			num = (int)strtol(optarg, NULL, 10);
+			loops = (int)strtol(optarg, NULL, 10);
 			break;
 
 		case 'd':
@@ -53,40 +55,42 @@ int main(int argc, char **argv)
 			break;
 
 		default:
-			num = 99;
 			break;
 		}
 	}
 
 	printf("\n");
 
-	for (b = num; b >= 0; b--) {
-		switch (b) {
-		case 0:
-			printf("No more bottles of beer on the wall, ");
-			printf("no more bottles of beer.\n");
-			printf("Go to the store and buy some more, ");
-			printf("99 bottles of beer on the wall.\n\n");
-			break;
+	while (loops > 0) {
+		for (b = 99; b >= 0; b--) {
+			switch (b) {
+			case 0:
+				printf("No more bottles of beer on the wall, ");
+				printf("no more bottles of beer.\n");
+				printf("Go to the store and buy some more, ");
+				printf("99 bottles of beer on the wall.\n\n");
+				break;
 
-		case 1:
-			printf("1 bottle of beer on the wall, ");
-			printf("1 bottle of beer.\n");
-			printf("If this bottle should happen to fall, no more");
-			printf("bottles of beer on the wall\n\n");
-			break;
+			case 1:
+				printf("1 bottle of beer on the wall, ");
+				printf("1 bottle of beer.\n");
+				printf("If this bottle should happen to fall, no more");
+				printf("bottles of beer on the wall\n\n");
+				break;
 
-		default:
-			printf("%d bottles of beer on the wall, ", b);
-			printf("%d bottles of beer.\n", b);
-			printf("If one of those bottles should happen to fall, ");
-			printf("%d %s of beer on the wall.\n\n", b - 1,
-			       ((b - 1) > 1) ? "bottles" : "bottle");
-			break;
+			default:
+				printf("%d bottles of beer on the wall, ", b);
+				printf("%d bottles of beer.\n", b);
+				printf("If one of those bottles should happen to fall, ");
+				printf("%d %s of beer on the wall.\n\n", b - 1,
+				       ((b - 1) > 1) ? "bottles" : "bottle");
+				break;
+			}
+
+			if (sleeptime > 0)
+				sleep(sleeptime);
 		}
-
-		if (sleeptime > 0)
-			sleep(sleeptime);
+		loops--;
 	}
 	return 0;
 }
